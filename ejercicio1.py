361d23b424f0694737da26ae661da2b138d8eaa3
jefburg <you@example.com>
2024-10-13 14:14:46 -0500

    Read a Download Excel

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,13 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+import xlwings as xw
+import os
+import glob
 import time
-
 
 # Iniciar el navegador
 driver = webdriver.Chrome()
 
-# Ir a la página web donde se realiza la descarga
+# Ir a la página web
 driver.get('https://docs.google.com/spreadsheets/d/14lrWudL3gf6mXHeUJCgqRDUkhGrx2BAOfw1e4TxO-H8/edit?usp=sharing')
 time.sleep(1)
 driver.find_element(By.ID, "docs-file-menu").click()
@@ -16,6 +18,31 @@ enlace_privacidad = driver.find_element(By.CSS_SELECTOR, 'div.apps-menuitem .goo
 action = ActionChains(driver)
 action.move_to_element(enlace_privacidad).perform()
 time.sleep(5)
-
+elemento_excel = driver.find_element(By.CSS_SELECTOR, 'div.apps-menuitem span[aria-label="Microsoft Excel (.xlsx) x"]')
+elemento_excel.click()
+time.sleep(7)
 # Cerrar el navegador
 driver.quit()
+#ruta_archivo = 'C:\\Users\\USER\\Desktop\\archivo1.xlsx'
+# Ruta de la carpeta de descargas (ajústala según tu sistema)
+carpeta_descargas = 'C:\\Users\\USER\\Downloads'
+
+# Buscar el archivo Excel más reciente en la carpeta de descargas
+lista_archivos = glob.glob(os.path.join(carpeta_descargas, '*.xlsx'))
+archivo_reciente = max(lista_archivos, key=os.path.getctime)
+
+print(f"El archivo más reciente es: {archivo_reciente}")
+
+wb = xw.Book(archivo_reciente)
+hoja = wb.sheets['Hoja 1']
+
+hoja['B1'].value = 1
+hoja['B2'].value = 1
+hoja['B3'].value = 1
+
+valor_b3 = hoja['B4'].value
+
+print(f"La suma es: {valor_b3}")
+
+wb.save(archivo_reciente)
+wb.close()
--- main.py
+++ main.py
@@ -1,24 +1,3 @@
-#import openpyxl
-
-## Cargar el archivo Excel (asegúrate de colocar la ruta correcta si está en tu escritorio)
-#ruta_archivo = 'C:\\Users\\USER\\Desktop\\archivo1.xlsx'  # Reemplaza 'TuUsuario' por tu nombre de usuario
-#wb = openpyxl.load_workbook(ruta_archivo)
-
-## Seleccionar la hoja activa (si hay más de una hoja, podrías especificar el nombre)
-#hoja = wb['Hoja2']
-
-## Escribir "PRACTICA" en la celda A1
-#hoja['B1'] = 1
-#hoja['B2'] = 3
-
-#wb.save(ruta_archivo)
-#wb = openpyxl.load_workbook(ruta_archivo, data_only=True)
-#hoja = wb['Hoja2']
-### Leer el valor de la celda A2
-#valor_a2 = hoja['B3'].value
-
-### Imprimir el valor de A2 en pantalla
-#print(f"La suma es: {valor_a2}")
 import xlwings as xw
 
 # Cargar el archivo Excel
